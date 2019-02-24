1b0e0c676d0f7b36033e2e3fc2ebafd4de3c945e
Istvan von Fedak <istvan.vonfedak@gmail.com>
2019-02-23 23:37:07 -0500

    added comments to Labeler.*

--- NaiveBayesianClassifier/Labeler.cpp
+++ NaiveBayesianClassifier/Labeler.cpp
@@ -6,46 +6,52 @@ Labeler::Labeler(const Data & randData, const unsigned trainingIndex, const Prob
   this->interpolate(randData, trainingIndex, trainingSet);
 }
 
+// resets class
 void Labeler::reset() {
   this->percentError = 0;
 }
 
+// returns the label of the entry given a probability training set
 std::string Labeler::label(const Probability & trainingSet, const StrVec & entry) {
   double demCondProb = 1; // defailt multiplicative identity
   double repCondProb = 1; // will contain the conditional probability of a republican
  // iterates through the entire training data
- for(unsigned int i = 1; i < trainingSet.vecSize; i++)
-  { if(entry[i] == "y") // updates the conditional probablitity of a democrat and republican if attribute is yes
-    { demCondProb *= trainingSet[i].demYes;
-      repCondProb *= trainingSet[i].repYes; }
-    else if(entry[i] == "n")
-    { demCondProb *= trainingSet[i].demNo;
-      repCondProb *= trainingSet[i].repNo; }
-    else if(entry[i] == "?")
-    { demCondProb *= trainingSet[i].demAbs;
-      demCondProb *= trainingSet[i].repAbs; }
-    else // if there's an error in data notify user
-    { fprintf(stderr, "Error:: labler():: entry[%u] ", i);
+  for(unsigned int i = 1; i < trainingSet.vecSize; i++) {
+    if(entry[i] == "y") { // updates the conditional probablitity of a democrat and republican if attribute is yes
+      demCondProb *= trainingSet[i].demYes;
+      repCondProb *= trainingSet[i].repYes;
+    } else if(entry[i] == "n") {
+      demCondProb *= trainingSet[i].demNo;
+      repCondProb *= trainingSet[i].repNo;
+    } else if(entry[i] == "?") {
+      demCondProb *= trainingSet[i].demAbs;
+      demCondProb *= trainingSet[i].repAbs;
+    } else { // if there's an error in data notify user
+      fprintf(stderr, "Error:: labler():: entry[%u] ", i);
       fprintf(stderr, "has invalid attribute name: %s\n", entry[i].c_str());
-      exit(1); } }
-    // naive bayesian classifier formula determining what class is the example
-    if (demCondProb * trainingSet.demProb < repCondProb * trainingSet.repProb)
-      return "republican";
-    else 
-      return "democrat"; }
+      exit(1);
+    }
+  }
+  // naive bayesian classifier formula determining what class is the example
+  if (demCondProb * trainingSet.demProb < repCondProb * trainingSet.repProb)
+    return "republican";
+  else
+    return "democrat";
+}
 
 // calculates the percent error for the given testing data by comparing if each example is true
-void Labeler::interpolate(const Data & randData, const unsigned trainingIndex, const Probability & trainingSet)
-{ int correct = 0; // stores the number of incorrectly classified examples in the testing data
+void Labeler::interpolate(const Data & randData, const unsigned trainingIndex, const Probability & trainingSet) {
+  int correct = 0; // stores the number of incorrectly classified examples in the testing data
   int incorrect = 0;
   // if there's no training data, display error
-  if(randData.size() <= trainingIndex)
-  { fprintf(stderr, "Error:: Frequency::interpolation():: trainingIndex is larger than or equal");
+  if(randData.size() <= trainingIndex) {
+    fprintf(stderr, "Error:: Frequency::interpolation():: trainingIndex is larger than or equal");
     fprintf(stderr, " to size of randData: %lu >= %u\n", randData.size(), trainingIndex);
-    exit(1); }
+    exit(1);
+  }
   // iterate through all the training data
-  for(unsigned int i = trainingIndex; i < randData.size(); i++)
-  { std::string result = Labeler::label(trainingSet, randData[i]);
+  for(unsigned int i = trainingIndex; i < randData.size(); i++) {
+    std::string result = Labeler::label(trainingSet, randData[i]);
     if(result == randData[i][0]) // compares to see if they match
       correct += 1;
     else
@@ -55,5 +61,6 @@ void Labeler::interpolate(const Data & randData, const unsigned trainingIndex, c
   this->percentError = (incorrect / ((double) correct + incorrect)) * 100;
 }
 
-void Labeler::print()
-{ printf("Percent error = %f\n", this->percentError); }
+void Labeler::print() {
+  printf("Percent error = %f\n", this->percentError); 
+}
