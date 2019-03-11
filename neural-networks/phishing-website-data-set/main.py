85f2d70e0bf790304e2251d51902da8e56480d78
Istvan von Fedak <istvan.vonfedak@gmail.com>
2019-03-11 18:56:09 -0400

    Cleaned up data.txt

--- neural-networks/phishing-website-data-set/main.py
+++ neural-networks/phishing-website-data-set/main.py
@@ -1,3 +1,12 @@
+# allows to import NeuralNetwork lib from different dir
+import sys
+
+# inserts path to access NeuralNetwork lib
+sys.path.insert(0, '../src')
+
 from NeuralNetwork import NeuralNetwork
+
+
+
 
 print("Done!")
